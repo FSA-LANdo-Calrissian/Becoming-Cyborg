4bf35064096a75775eef1b894cee56524ec1672b
Will Barksdale <thefithh@gmail.com>
2021-03-17 18:29:35 -0400

    added enemy tracking

---
+++ src/entity/Enemy.js
@@ -0,0 +1,20 @@
+import Phaser from 'phaser';
+
+export default class Enemy extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y, spriteKey) {
+    super(scene, x, y, spriteKey);
+    this.scene = scene;
+    this.scene.physics.world.enable(this);
+    this.scene.add.existing(this);
+    this.body.setAllowGravity(false);
+    this.speed = 80;
+    this.health = 100;
+  }
+
+  enemyMovement(direction) {
+    switch (direction) {
+      case 'left':
+        this.play('enemyRunLeft');
+    }
+  }
+}
--- src/scenes/FgScene.js
+++ src/scenes/FgScene.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import Player from '../entity/Player';
+import Enemy from '../entity/Enemy';
 
 export default class FgScene extends Phaser.Scene {
   constructor() {
@@ -10,6 +11,10 @@ export default class FgScene extends Phaser.Scene {
     this.load.spritesheet('player', '../../public/assets/sprites/cyborg.png', {
       frameWidth: 47,
       frameHeight: 50,
+    });
+    this.load.spritesheet('enemy', '../../public/assets/sprites/Walk.png', {
+      frameWidth: 41,
+      frameHeight: 51,
     });
   }
 
@@ -77,10 +82,75 @@ export default class FgScene extends Phaser.Scene {
       frameRate: 0,
       repeat: 0,
     });
+
+    this.anims.create({
+      key: 'enemyRunLeft',
+      frames: this.anims.generateFrameNumbers('enemy', {
+        start: 9,
+        end: 11,
+      }),
+      frameRate: 7,
+      repeat: -1,
+      yoyo: true,
+    });
+
+    this.anims.create({
+      key: 'enemyRunRight',
+      frames: this.anims.generateFrameNumbers('enemy', {
+        start: 3,
+        end: 5,
+      }),
+      frameRate: 7,
+      repeat: -1,
+      yoyo: true,
+    });
+
+    this.anims.create({
+      key: 'enemyRunUp',
+      frames: this.anims.generateFrameNumbers('enemy', {
+        start: 6,
+        end: 8,
+      }),
+      frameRate: 5,
+      repeat: -1,
+      yoyo: true,
+    });
+
+    this.anims.create({
+      key: 'enemyRunDown',
+      frames: this.anims.generateFrameNumbers('enemy', {
+        start: 0,
+        end: 2,
+      }),
+      frameRate: 5,
+      repeat: -1,
+      yoyo: true,
+    });
+
+    this.anims.create({
+      key: 'enemyIdleRight',
+      frames: this.anims.generateFrameNumbers('enemy', {
+        start: 4,
+        end: 4,
+      }),
+      frameRate: 0,
+      repeat: 0,
+    });
+
+    this.anims.create({
+      key: 'enemyIdleLeft',
+      frames: this.anims.generateFrameNumbers('enemy', {
+        start: 10,
+        end: 10,
+      }),
+      frameRate: 0,
+      repeat: 0,
+    });
   }
 
   create() {
     this.player = new Player(this, 20, 400, 'player').setScale(2);
+    this.enemy = new Enemy(this, 760, 400, 'enemy').setScale(2);
 
     this.cursors = this.input.keyboard.addKeys({
       up: Phaser.Input.Keyboard.KeyCodes.W,
@@ -92,7 +162,43 @@ export default class FgScene extends Phaser.Scene {
     this.createAnimations();
   }
 
+  updateEnemyMovement() {
+    if (
+      this.player.x - this.enemy.x >= 180 ||
+      this.player.x - this.enemy.x >= -180
+    ) {
+      // if player to left of enemy AND enemy moving to right (or not moving)
+      if (this.player.x < this.enemy.x && this.enemy.body.velocity.x >= 0) {
+        // move enemy to left
+        this.enemy.body.velocity.x = -150;
+        this.enemy.enemyMovement('left');
+      }
+      // if player to right of enemy AND enemy moving to left (or not moving)
+      else if (
+        this.player.x > this.enemy.x &&
+        this.enemy.body.velocity.x <= 0
+      ) {
+        // move enemy to right
+        this.enemy.body.velocity.x = 150;
+        this.enemy.enemyMovement('right');
+      } else if (
+        this.player.y < this.enemy.y &&
+        this.enemy.body.velocity.y >= 0
+      ) {
+        this.enemy.body.velocity.y = -150;
+        this.enemyMovement('up');
+      } else if (
+        this.player.y > this.enemy.y &&
+        this.enemy.body.velocity.y <= 0
+      ) {
+        this.enemy.body.velocity.y = 150;
+        this.enemyMovement('down');
+      }
+    }
+  }
+
   update(time, delta) {
     this.player.update(this.cursors);
+    this.updateEnemyMovement();
   }
 }
