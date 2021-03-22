5134f4bf87d431a7b6cf6cccba0622d581260e2f
PigsGoMoo <azianknowitall@gmail.com>
2021-03-22 18:24:36 -0400

    Chore: Added function docs for FgScene

--- src/scenes/FgScene.js
+++ src/scenes/FgScene.js
@@ -16,6 +16,11 @@ export default class FgScene extends Phaser.Scene {
   }
 
   openUpgrade() {
+    /*
+      Opens up the upgrade window for the player. Should only be accessed when player is at a workbench.
+      No params
+      returns null.
+    */
     this.scene.transition({
       target: 'UpgradeUI',
       sleep: true,
@@ -25,20 +30,40 @@ export default class FgScene extends Phaser.Scene {
   }
 
   loadBullet(x, y, sprite, angle) {
-    let bullet = this.playerProjectiles.getFirstDead();
+    /*
+      Function to pass into any entity that uses projectiles on creation.
+      This function will fire a newly created projectile or use a dead one with the appropriate key if available. Projectile logic will move the projectile to where it needs to go on its own.
+
+      param x: int -> X coordinate of whatever is shooting the projectile (not of the target).
+      param y: int -> Y coordinate of whatever is shooting the projectile (not of the target).
+      param sprite: string -> the key for the sprite to use.
+      param angle: The angle at which to shoot and render projectile.
+      returns null.
+    */
+
+    // Grab dead projectile from group if available.
+    let bullet = this.playerProjectiles.getFirstDead(false, x, y, sprite);
+    // If none found, create it.
     if (!bullet) {
-      console.log(`sprite`, sprite);
       bullet = new Projectile(this, x, y, sprite, angle).setScale(0.5);
+      // Add to projectiles group.
+      // TODO: Add logic for whether to add to player or enemy projectile group
       this.playerProjectiles.add(bullet);
-    } else {
-      console.log(`Bullet loaded`, bullet);
     }
+    // Pew pew the bullet.
     bullet.shoot(x, y, angle);
   }
 
   addText(i) {
+    /*
+      Function to advance the current dialogue. Destroys the dialogue box and allows player to move again on completion of dialogue.
+      param i: int -> The current index of the dialogue.
+      returns null
+    */
+
+    // If the dialogue is over (index higher than length)
     if (i > this.textLines.length - 1) {
-      console.log('FIGHT!!');
+      // Destroy box + allow movement.
       this.textBox.destroy();
       this.tutorialInProgress = false;
       this.finishedTutorial = true;
@@ -46,16 +71,27 @@ export default class FgScene extends Phaser.Scene {
       this.player.shooting = false;
       this.enemy.body.moves = true;
     }
+    // Advance the dialogue (this will also allow
+    // the text to be removed from screen)
     this.tutorialText.setText(this.textLines[i]);
   }
 
   playDialogue() {
+    /*
+      Tutorial dialogue. Contains the logic to advance through the dialogue on player clicking on the text.
+      Can increase the click area by changing the setInteractive rectangle width/height.
+      No params.
+      Returns null.
+    */
+
+    // Make the text box
     this.textBox = this.add.image(
       this.player.x - 10,
       this.player.y + 50,
       'textBox'
     );
     this.textBox.setScale(0.09);
+    // Lines to display in conversation.
     this.textLines = [
       'Halt human, stop right there!',
       'Name...?',
@@ -70,7 +106,10 @@ export default class FgScene extends Phaser.Scene {
       'Come with me human you are being detained for questioning.....',
       'Please do not resist....',
     ];
+
+    // Initialize index.
     let i = 0;
+    // Add text.
     this.tutorialText = this.add.text(
       this.textBox.x,
       this.textBox.y + 2,
@@ -85,6 +124,9 @@ export default class FgScene extends Phaser.Scene {
     this.tutorialText.setResolution(10);
     this.tutorialText.setScale(0.4).setOrigin(0.5);
 
+    // Add click area to advance text. Change the numbers after
+    // the tutorialText width/height in order to increase click
+    // area.
     this.tutorialText.setInteractive(
       new Phaser.Geom.Rectangle(
         0,
@@ -94,49 +136,54 @@ export default class FgScene extends Phaser.Scene {
       ),
       Phaser.Geom.Rectangle.Contains
     );
+
+    // Emit this so that the text doesn't show up on minimap
     this.events.emit('dialogue');
 
+    // Freeze enemy and player movement.
     this.player.body.moves = false;
     this.player.shooting = true;
     this.enemy.body.moves = false;
+
+    // Add the listener for mouse click.
     this.tutorialText.on('pointerdown', () => {
       this.addText(i);
       i++;
     });
   }
 
+  damageEnemy(enemy, projectile) {
+    /*
+      Logic to damage enemy. Checks if enemy isn't dead and if whatever is doing the damage is active before doing damage.
+
+      param enemy: object -> enemy that's being damaged
+      param projectile: object -> Thing doing the damage (must have the this.damage property on it)
+      returns null.
+    */
+    if (enemy.active === true && projectile.active === true) {
+      projectile.destroy();
+
+      enemy.takeDamage(projectile.damage);
+    }
+  }
+
   create(data) {
+    // Initializing the game.
     this.finishedTutorial = false;
     this.cameras.main.fadeIn(2000, 0, 0, 0);
-    // this.scene.launch('HUDScene');
     this.gg = this.sound.add('gg');
-    //map stuff
+
+    //Load in map stuff
     this.map = this.make.tilemap({ key: 'map' });
 
     this.darkGrass = this.map.addTilesetImage('forest', 'forest');
-
-    // const earthyTiles = this.map.addTilesetImage('sci-fi', 'earthy-tiles');
 
     this.grassAndBuildings = this.map.addTilesetImage(
       'apocalypse',
       'apocalypse'
     );
 
-    // const extraBuildings = this.map.addTilesetImage(
-    //   'apocalypse-extra',
-    //   'extra-buildings'
-    // );
-
     this.belowLayer1 = this.map.createLayer('ground', this.darkGrass, 0, 0);
-
-    // const belowLayer2 = this.map.createStaticLayer('ground', earthyTiles, 0, 0);
-
-    // const belowLayer3 = this.map.createStaticLayer(
-    //   'ground',
-    //   grassAndBuildings,
-    //   0,
-    //   0
-    // );
 
     this.worldLayer1 = this.map.createLayer(
       'above-ground',
@@ -145,30 +192,22 @@ export default class FgScene extends Phaser.Scene {
       0
     );
 
-    // const worldLayer2 = this.map.createStaticLayer(
-    //   'above-ground',
-    //   extraBuildings,
-    //   0,
-    //   0
-    // );
-
     this.worldLayer1.setCollisionByProperty({ collides: true });
 
-    // const debugGraphics = this.add.graphics().setAlpha(0.75);
-    // this.worldLayer1.renderDebug(debugGraphics, {
-    //   tileColor: null, // Color of non-colliding tiles
-    //   collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
-    //   faceColor: new Phaser.Display.Color(40, 39, 37, 255), // Color of colliding face edges
-    // });
+    // Show debug collisions on the map.
+    const debugGraphics = this.add.graphics().setAlpha(0.75);
+    this.worldLayer1.renderDebug(debugGraphics, {
+      tileColor: null, // Color of non-colliding tiles
+      collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+      faceColor: new Phaser.Display.Color(40, 39, 37, 255), // Color of colliding face edges
+    });
 
-    // worldLayer2.setCollisionByProperty({ collides: true });
-
-    // Spawning the player
-
+    // Spawning the entities
     this.player = new Player(this, 38, 23, 'player', this.loadBullet).setScale(
       0.3
     );
     this.enemy = new Enemy(this, 473, 176, 'enemy').setScale(0.4);
+
     // Groups
     this.playerProjectiles = this.physics.add.group({
       classType: Projectile,
@@ -183,7 +222,6 @@ export default class FgScene extends Phaser.Scene {
 
     // Collision logic
     this.physics.add.collider(this.player, this.worldLayer1);
-    // this.physics.add.collider(this.player, this.enemy);
     this.physics.add.overlap(this.player, this.enemy, () => {
       this.player.takeDamage(10, this.gg);
     });
@@ -225,42 +263,18 @@ export default class FgScene extends Phaser.Scene {
       this.scene.wake();
     });
 
+    // data.choice is only available when player restarts game.
     if (data.choice) {
       this.scene.restart({ choice: false });
       const main = this.scene.get('MainScene');
       main.scene.restart({ choice: false });
     }
-
-    // this.tutorial = true;
-
-    // let continueText = this.add.text(
-    //   this.textBox.x + 20,
-    //   this.textBox.y + 10,
-    //   'Press here to continue'
-    // );
-    // continueText
-    //   .setResolution(10)
-    //   .setScale(0.23)
-    //   .setOrigin(0.5)
-    //   .setInteractive(
-    //     new Phaser.Geom.Rectangle(0, 0, text.width, text.height),
-    //     Phaser.Geom.Rectangle.Contains
-    //   )
-    //   .on('pointerdown', function addText() {
-    //     i++;
-    //     text.setText(textLines[i]);
-    //     console.log(textLines[i]);
-
-    //     console.log(i);
-    //   });
-
-    // var container = this.add.container(this.player.x + 70, this.player.y - 50, [
-    //   this.textBox,
-    //   text,
-    // ]);
   }
 
   update(time, delta) {
+    // Tutorial logic - if player hasn't talked to enemy robot
+    // yet and is within a range of 51 of the robot, initialize
+    // talking.
     if (
       !this.tutorialInProgress &&
       !this.finishedTutorial &&
@@ -272,7 +286,7 @@ export default class FgScene extends Phaser.Scene {
       ) < 51
     ) {
       this.tutorialInProgress = true;
-
+      // stop animations
       this.player.play(
         this.player.facingRight ? 'idleRight' : 'idleLeft',
         true
@@ -280,6 +294,7 @@ export default class FgScene extends Phaser.Scene {
       this.playDialogue();
     }
 
+    // If not in dialogue, allow player to move with cursors.
     if (!this.tutorialInProgress) {
       this.player.update(this.cursors, time);
       this.enemy.update(this.player);
@@ -289,14 +304,6 @@ export default class FgScene extends Phaser.Scene {
       if (this.cursors.hp.isDown) {
         console.log(this.playerProjectiles);
       }
-    }
-  }
-
-  damageEnemy(enemy, projectile) {
-    if (enemy.active === true && projectile.active === true) {
-      projectile.destroy();
-
-      enemy.takeDamage(projectile.damage);
     }
   }
 }
