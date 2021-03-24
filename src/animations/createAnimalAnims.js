7ad4e3a3ac91e0962105adc0c8f7672f45dc1551
Adam Kopell <aakopell@gmail.com>
2021-03-23 21:04:34 -0700

    fix: combined wolf spritesheets and fixed hitbox

--- src/animations/createAnimalAnims.js
+++ src/animations/createAnimalAnims.js
@@ -3,8 +3,8 @@ export default function createAnimalAnims() {
   this.anims.create({
     key: 'wolfRunLeft',
     frames: this.anims.generateFrameNumbers('wolf', {
-      start: 50,
-      end: 54,
+      start: 40,
+      end: 44,
     }),
     frameRate: 7,
     repeat: -1,
@@ -12,8 +12,8 @@ export default function createAnimalAnims() {
   this.anims.create({
     key: 'wolfRunRight',
     frames: this.anims.generateFrameNumbers('wolf', {
-      start: 20,
-      end: 24,
+      start: 15,
+      end: 19,
     }),
     frameRate: 7,
     repeat: -1,
@@ -21,8 +21,8 @@ export default function createAnimalAnims() {
   this.anims.create({
     key: 'wolfRunUp',
     frames: this.anims.generateFrameNumbers('wolf', {
-      start: 35,
-      end: 39,
+      start: 85,
+      end: 89,
     }),
     frameRate: 7,
     repeat: -1,
@@ -30,8 +30,8 @@ export default function createAnimalAnims() {
   this.anims.create({
     key: 'wolfRunDown',
     frames: this.anims.generateFrameNumbers('wolf', {
-      start: 30,
-      end: 34,
+      start: 80,
+      end: 84,
     }),
     frameRate: 7,
     repeat: -1,
@@ -43,24 +43,6 @@ export default function createAnimalAnims() {
   this.anims.create({
     key: 'wolfAttackLeft',
     frames: this.anims.generateFrameNumbers('wolf', {
-      start: 55,
-      end: 59,
-    }),
-    frameRate: 7,
-    repeat: -1,
-  });
-  this.anims.create({
-    key: 'wolfAttackRight',
-    frames: this.anims.generateFrameNumbers('wolf', {
-      start: 25,
-      end: 29,
-    }),
-    frameRate: 7,
-    repeat: -1,
-  });
-  this.anims.create({
-    key: 'wolfAttackUp',
-    frames: this.anims.generateFrameNumbers('wolf', {
       start: 45,
       end: 49,
     }),
@@ -68,12 +50,30 @@ export default function createAnimalAnims() {
     repeat: -1,
   });
   this.anims.create({
-    key: 'wolfAttackDown',
+    key: 'wolfAttackRight',
     frames: this.anims.generateFrameNumbers('wolf', {
-      start: 40,
-      end: 44,
+      start: 20,
+      end: 24,
     }),
     frameRate: 7,
+    repeat: -1,
+  });
+  this.anims.create({
+    key: 'wolfAttackUp',
+    frames: this.anims.generateFrameNumbers('wolf', {
+      start: 95,
+      end: 98,
+    }),
+    frameRate: 5,
+    repeat: -1,
+  });
+  this.anims.create({
+    key: 'wolfAttackDown',
+    frames: this.anims.generateFrameNumbers('wolf', {
+      start: 90,
+      end: 93,
+    }),
+    frameRate: 5,
     repeat: -1,
   });
 }
