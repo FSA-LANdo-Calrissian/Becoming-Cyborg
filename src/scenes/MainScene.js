28d3cd31670ace10f9b967ec26db6d4cf6b88b2c
PigsGoMoo <azianknowitall@gmail.com>
2021-03-31 18:56:51 -0400

    Feat: Added a boss room

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -8,8 +8,7 @@ export default class MainScene extends Phaser.Scene {
   }
 
   create() {
-    // this.scene.launch('FgScene');
-    // this.scene.launch('HUDScene');
-    this.scene.launch('BossScene');
+    this.scene.launch('FgScene');
+    this.scene.launch('HUDScene');
   }
 }
